cead3837aa0474f0a7090a7f2dbd0ed0ee3a01a2
TyronRusty <Ktyred@gmail.com>
2023-02-10 20:55:10 -0500

    login button working

--- JS/myscript.js
+++ JS/myscript.js
@@ -1,22 +1,36 @@
-const form = document.getElementById("signup-form");
-
-form.addEventListener("submit", function(event) {
-  event.preventDefault();
-  
-  const username = document.getElementById("username").value;
-  const email = document.getElementById("email").value;
-  const password = document.getElementById("password").value;
-  
-  console.log(`Username: ${username}`);
-  console.log(`Email: ${email}`);
-  console.log(`Password: ${password}`);
-});
-// Get the modal
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
+// use to clear local storage
+         // localStorage.clear()
+      
+          // Use to load usernames to your local storage
+          const loadUsers = () => {
+            const users = ["John", "James", "Paul"];
+            // Use to loop through users
+            users.forEach((name) => {
+                // Sets the key and value
+                localStorage.setItem(name, name);
+            });
+            // Sets an item to your local storage, the key and value
+            localStorage.setItem("password", "password");
+        };
+    
+        const loginForm = document.getElementById("signup-form");
+        const loginButton = document.getElementById("loginButton");
+    
+        loginButton.addEventListener("click", (e) => {
+            e.preventDefault();
+            const userName = loginForm.userText.value;
+            const userPassword = loginForm.userPassword.value;
+    
+            // check if the input and local storage are the same
+            if (localStorage.getItem(userName) === userName &&
+                localStorage.getItem(userPassword) === "password"){
+                localStorage.setItem("name",userName);
+                window.location.replace("https://www.youtube.com");
+            } else {
+                // stop the page from submitting
+                e.preventDefault();
+                console.log("Please try again");
+                console.log(userName);
+                console.log(userPassword);
+            }
+        });
